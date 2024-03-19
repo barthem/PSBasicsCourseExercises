b0e6d82ad7abeb5795d56d20b08e56bb341dcadb
barthem <bart-de-langen@hotmail.com>
2024-03-19 14:00:39 +0100

    Update 11. filteren met logica.ps1

--- part 2/11. filteren met logica.ps1
+++ part 2/11. filteren met logica.ps1
@@ -7,5 +7,5 @@ get-service |where-object {$_.status -eq "running"}
 
 #weergeef enkel de services die momenteel running zijn, en startup type "manuel" is
 #voor deze statement is extra parenthesis nodig, omdat powershell anders niet weet wat die eerste moet checken
-get-service |Select-Object displayname, status, starttype  |where-object {($_.status -eq "running") -and (($_.starttype -eq "Manuel"))} | out-gridview
+get-service |Select-Object displayname, status, starttype  |where-object {$_.status -eq "running" -and $_.starttype -eq "Manuel"} | out-gridview
 
